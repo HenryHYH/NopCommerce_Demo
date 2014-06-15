cf967cf95a25e2c5b245290004d8ec31a4caa4a5
HenryHYH <henry.hyh@gmail.com>
2014-06-15 10:59:00 +0800

    添加 Tip Widget

---
+++ Plugins/Nop.Plugin.Widgets.Tip/Controllers/WidgetsTipController.cs
@@ -0,0 +1,76 @@
+﻿using System.Web.Mvc;
+using Nop.Core;
+using Nop.Core.Caching;
+using Nop.Plugin.Widgets.Tip.Models;
+using Nop.Services.Configuration;
+using Nop.Services.Media;
+using Nop.Services.Stores;
+using Nop.Web.Framework.Controllers;
+
+namespace Nop.Plugin.Widgets.Tip.Controllers
+{
+    public class WidgetsTipController : BasePluginController
+    {
+        private readonly IWorkContext _workContext;
+        private readonly IStoreContext _storeContext;
+        private readonly IStoreService _storeService;
+        private readonly ISettingService _settingService;
+
+        public WidgetsTipController(IWorkContext workContext,
+            IStoreContext storeContext,
+            IStoreService storeService,
+            ISettingService settingService)
+        {
+            this._workContext = workContext;
+            this._storeContext = storeContext;
+            this._storeService = storeService;
+            this._settingService = settingService;
+        }
+
+        [AdminAuthorize]
+        [ChildActionOnly]
+        public ActionResult Configure()
+        {
+            //load settings for a chosen store scope
+            var storeScope = this.GetActiveStoreScopeConfiguration(_storeService, _workContext);
+            var tipSettings = _settingService.LoadSetting<TipSettings>(storeScope);
+            var model = new ConfigurationModel();
+
+            if (storeScope > 0)
+            {
+
+            }
+
+            return View("Nop.Plugin.Widgets.Tip.Views.WidgetsTip.Configure", model);
+        }
+
+        [HttpPost]
+        [AdminAuthorize]
+        [ChildActionOnly]
+        public ActionResult Configure(ConfigurationModel model)
+        {
+            //load settings for a chosen store scope
+            var storeScope = this.GetActiveStoreScopeConfiguration(_storeService, _workContext);
+            var tipSettings = _settingService.LoadSetting<TipSettings>(storeScope);            
+
+            /* We do not clear cache after each setting update.
+             * This behavior can increase performance because cached settings will not be cleared 
+             * and loaded from database after each update */
+            
+            //now clear settings cache
+            _settingService.ClearCache();
+            
+            return Configure();
+        }
+
+        [ChildActionOnly]
+        public ActionResult PublicInfo(string widgetZone)
+        {
+            var tipSettings = _settingService.LoadSetting<TipSettings>(_storeContext.CurrentStore.Id);
+
+            var model = new PublicInfoModel();
+
+            return View("Nop.Plugin.Widgets.Tip.Views.WidgetsTip.PublicInfo", model);
+        }
+    }
+}
---
+++ Plugins/Nop.Plugin.Widgets.Tip/TipPlugin.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Routing;
+using Nop.Core;
+using Nop.Core.Plugins;
+using Nop.Services.Cms;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Services.Media;
+
+namespace Nop.Plugin.Widgets.Tip
+{
+    /// <summary>
+    /// PLugin
+    /// </summary>
+    public class TipPlugin : BasePlugin, IWidgetPlugin
+    {
+        private readonly ISettingService _settingService;
+
+        public TipPlugin(ISettingService settingService)
+        {
+            this._settingService = settingService;
+        }
+
+        /// <summary>
+        /// Gets widget zones where this widget should be rendered
+        /// </summary>
+        /// <returns>Widget zones</returns>
+        public IList<string> GetWidgetZones()
+        {
+            return new List<string>() { "message_tip" };
+        }
+
+        /// <summary>
+        /// Gets a route for provider configuration
+        /// </summary>
+        /// <param name="actionName">Action name</param>
+        /// <param name="controllerName">Controller name</param>
+        /// <param name="routeValues">Route values</param>
+        public void GetConfigurationRoute(out string actionName, out string controllerName, out RouteValueDictionary routeValues)
+        {
+            actionName = "Configure";
+            controllerName = "WidgetsTip";
+            routeValues = new RouteValueDictionary() { { "Namespaces", "Nop.Plugin.Widgets.Tip.Controllers" }, { "area", null } };
+        }
+
+        /// <summary>
+        /// Gets a route for displaying widget
+        /// </summary>
+        /// <param name="widgetZone">Widget zone where it's displayed</param>
+        /// <param name="actionName">Action name</param>
+        /// <param name="controllerName">Controller name</param>
+        /// <param name="routeValues">Route values</param>
+        public void GetDisplayWidgetRoute(string widgetZone, out string actionName, out string controllerName, out RouteValueDictionary routeValues)
+        {
+            actionName = "PublicInfo";
+            controllerName = "WidgetsTip";
+            routeValues = new RouteValueDictionary()
+            {
+                {"Namespaces", "Nop.Plugin.Widgets.Tip.Controllers"},
+                {"area", null},
+                {"widgetZone", widgetZone}
+            };
+        }
+
+        /// <summary>
+        /// Install plugin
+        /// </summary>
+        public override void Install()
+        {
+            //settings
+            var settings = new TipSettings();
+            _settingService.SaveSetting(settings);
+
+            base.Install();
+        }
+
+        /// <summary>
+        /// Uninstall plugin
+        /// </summary>
+        public override void Uninstall()
+        {
+            //settings
+            _settingService.DeleteSetting<TipSettings>();
+
+            base.Uninstall();
+        }
+    }
+}
